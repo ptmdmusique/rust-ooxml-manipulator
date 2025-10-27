6e7312e63fee8d4812dbfd8ccca26d3501a99055
ptmdmusique <ptmdmusic@gmail.com>
2025-10-27 00:18:35 -0700

    feat: add file extension and util to handle files

---
+++ src/utils/files.rs
@@ -0,0 +1,52 @@
+use crate::utils::types::FilePathInfo;
+use crate::utils::types::ZipFolder;
+use serde::Serialize;
+use std::fs::{self, DirEntry};
+use std::io;
+use std::path::Path;
+
+/// Get the output folder for the zip file extraction including the root folder and the folder that the Word file will be extracted to
+pub fn get_output_folder(file_path_info: &FilePathInfo) -> ZipFolder {
+    let FilePathInfo {
+        file_name,
+        file_path,
+        ..
+    } = file_path_info;
+
+    let output_folder = format!("{}/{}", file_path, file_name);
+    let zip_output_folder = format!("{}/{}", output_folder, "extracted");
+
+    ZipFolder {
+        root_folder: output_folder,
+        extracted_folder: zip_output_folder,
+    }
+}
+
+/// https://doc.rust-lang.org/nightly/std/fs/fn.read_dir.html#examples
+pub fn visit_dirs(dir: &Path, file_callback: &mut dyn FnMut(&DirEntry)) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                visit_dirs(&path, file_callback)?;
+            } else {
+                file_callback(&entry);
+            }
+        }
+    }
+    Ok(())
+}
+
+/// Check whether the given extension is an image extension
+pub fn is_image_extension(extension: &str) -> bool {
+    matches!(
+        extension,
+        "jpg" | "jpeg" | "png" | "gif" | "bmp" | "tiff" | "ico"
+    )
+}
+
+pub fn write_struct_to_json<T: Serialize>(data: &T, file_path: &str) -> Result<(), std::io::Error> {
+    let json = serde_json::to_string_pretty(data)?;
+    fs::write(file_path, json)
+}
--- src/utils/types.rs
+++ src/utils/types.rs
@@ -7,10 +7,11 @@ use std::path::Path;
 pub struct FilePathInfo {
     /// Name of the file without extension
     pub file_name: String,
-    pub file_extension: String,
+    pub file_extension: Option<String>,
     /// File path without the file name
     pub file_path: String,
     pub full_file_path: String,
+    pub file_size: u64,
 }
 
 impl FilePathInfo {
@@ -26,30 +27,46 @@ impl FilePathInfo {
             )),
         };
 
-        let file_extension = match path.extension() {
-            Some(extension) => extension.to_str().unwrap().to_string(),
-            None => {
-                print_error_with_panic(&format!("File extension is not valid {}", full_file_path))
-            }
+        let file_extension = path
+            .extension()
+            .map(|extension| extension.to_string_lossy().to_string());
+        let file_size = match path.metadata() {
+            Ok(metadata) => metadata.len(),
+            Err(e) => print_error_with_panic(&format!("Failed to get file size: {}", e)),
         };
 
-        let file_name = full_file_name.split('.').next().unwrap().to_string();
+        let file_name = match full_file_name.split('.').next() {
+            Some(file_name) => file_name.to_string(),
+            None => print_error_with_panic(&format!(
+                "File name doesn't have a extension: {}",
+                full_file_path
+            )),
+        };
 
-        let file_path_without_file_name = path.parent().unwrap().to_str().unwrap().to_string();
+        let file_path_without_file_name = match path.parent() {
+            Some(parent) => parent.to_str().unwrap().to_string(),
+            None => print_error_with_panic(&format!(
+                "File path doesn't have a parent: {}",
+                full_file_path
+            )),
+        };
 
         Self {
             file_name,
             file_extension,
             file_path: file_path_without_file_name,
             full_file_path,
+            file_size,
         }
     }
 
     pub fn print_info(&self) {
         println!("{}", "File information:".green());
+
+        let extension = self.file_extension.as_ref().unwrap();
         println!(
             "\tFull path: {}\n\tName: {}\n\tExtension: {}",
-            self.full_file_path, self.file_name, self.file_extension
+            self.full_file_path, self.file_name, extension
         );
     }
 }
